4f3de02c161fce39e3e4ae9580489c319960ff6d
Mahmoud Fansha <fanasheh@hotmail.com>
2020-09-15 23:39:06 -0700

    Basilica service

---
+++ web_app/services/basillca_service.py
@@ -0,0 +1,28 @@
+
+
+
+from basilica import Connection
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("BASILICA_API_KEY") 
+
+connection = Connection(API_KEY)
+print("CONNECTOIN", type(connection))
+
+if __name__ == "__main__":
+
+    sentences = [
+        "This is a sentence!",
+        "This is a similar sentence!",
+        "I don't think this sentence is very similar at all...",
+    ]
+    embeddings = list(connection.embed_sentences(sentences))
+    print(embeddings)
+
+    embedding = connection.embed_sentence("Hello world!!!", model="twitter")
+    print(type(embedding)) #> <class 'list>
+    print(type(embedding[0])) #> <class 'float'>
+    print(len(embedding)) #768
