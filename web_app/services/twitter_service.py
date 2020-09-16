f686e4206c10b0e4d75351687342cb043b1ede02
Mahmoud Fansha <fanasheh@hotmail.com>
2020-09-16 10:39:37 -0700

    Storing users and tweets in the database

--- web_app/services/twitter_service.py
+++ web_app/services/twitter_service.py
@@ -12,23 +12,26 @@ TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
 TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
 
 auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-
+print("API AUTH:", auth)
 api = API(auth)
-print("API CLINET", api)
-
-user = api.get_user('s2t2')
-print("TWITTER USER:", type(user))
-print(user.id)
-print(user.screen_name)
-print(user.name)
+print("API CLINET:", api)
 
 
-tweets = api.user_timeline("s2t2", tweet_mode="extended")
-print("TWEETS", type(tweets))
-print(type(tweets[0]))
+if __name__ == "__main__":
+    
+    user = api.get_user('s2t2')
+    print("TWITTER USER:", type(user))
+    print(user.id)
+    print(user.screen_name)
+    print(user.name)
 
-tweet = tweets[0]
-print(tweet.id)
-print(tweet.full_text)
 
-breakpoint()
+    tweets = api.user_timeline("s2t2", tweet_mode="extended")
+    print("TWEETS", type(tweets))
+    print(type(tweets[0]))
+
+    tweet = tweets[0]
+    print(tweet.id)
+    print(tweet.full_text)
+
+    breakpoint()
