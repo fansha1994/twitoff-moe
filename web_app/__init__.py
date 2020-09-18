bfc3dd9182b264edc95f03f195003bd0783b8d47
Mahmoud Fansha <fanasheh@hotmail.com>
2020-09-17 22:45:01 -0700

    Flash messaging

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -6,10 +6,12 @@ from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
 DATABASE_URI = "sqlite:///twitoff_moe_development.db" # using relative filepath
+SECRET_KEY = "todo customize this secret value via env var"
 
 def create_app():
     app = Flask(__name__)
 
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
