56c9f9145bd73e67080fd15774b64aafd552eb56
Mahmoud Fansha <fanasheh@hotmail.com>
2020-09-18 00:51:54 -0700

    Configure db via env var

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask 
+import os
+from dotenv import load_dotenv
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = "sqlite:///twitoff_moe_development.db" # using relative filepath
-SECRET_KEY = "todo customize this secret value via env var"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = SECRET_KEY
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
